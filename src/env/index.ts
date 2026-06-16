536d3ea74c8527e7361c60784be418575ab235c6
jussie-lopes23 <152648620+jussie-lopes23@users.noreply.github.com>
2026-06-16 00:12:54 -0300

    feat: validando as variáveis de ambiente

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import {z} from 'zod'
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'test', 'production']). default('production'),
+    DATABSE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
+    DATABASE_URL: z.string(),
+    PORT: z.coerce.number().default(3333), 
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if(_env.success === false ){
+    console.error('Variáveis ​​de ambiente inválidas', _env.error.format())
+
+    throw new Error('Inavlid environment variables')
+}
+
+export const env = _env.data
